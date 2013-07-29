e3864c4ce85615df8bf326a9c492f98941293a49
giovanni1 <mikerubygiovanni@yahoo.com>
2013-07-29 15:07:45 -0700

    Create this is so not far.ino

---
+++ this is so not far.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+for( int i = 2; i<= 8 ; i++)
+{ 
+  pinMode(i,OUTPUT);
+} 
+  
+}
+
+void loop()
+{
+ for( int i = 2; i<= 8 ; i++)
+{ 
+   digitalWrite(i,HIGH);
+   delay(analogRead(A0);
+   digitalWrite(i,LOW);
+ }  
+}
+
+
