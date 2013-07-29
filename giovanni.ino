fbd0b338e22826eb55b805e1d84ac03ab9654e66
giovanni1 <mikerubygiovanni@yahoo.com>
2013-07-29 15:36:08 -0700

    Create giovanni.ino

---
+++ giovanni.ino
@@ -0,0 +1,22 @@
+void setup()
+{
+for( int i = 0; i<= 9
+ ; i++)
+{ 
+  pinMode(i,OUTPUT);
+} 
+  
+}
+
+void loop()
+{
+ for( int i = 0; i<= 9 ; i++)
+{ 
+   digitalWrite(i,HIGH);
+   tone(9,100*i,1000);
+   delay( analogRead(A0) );
+   digitalWrite(i,LOW);
+ }  
+}
+
+
