43e581ecf09a178c487dd9f919d99a378918f587
gskielian <gregory.kielian@hackerdojo.com>
2013-07-31 21:43:42 -0700

    Update KnobControlledLightSpeed.ino

--- music-and-lights/KnobControlledLightSpeed.ino
+++ music-and-lights/KnobControlledLightSpeed.ino
@@ -1,20 +1,23 @@
 void setup()
 {
-for( int i = 2; i<= 8 ; i++)
-{ 
-  pinMode(i,OUTPUT);
-} 
+ 
+  for( int i = 2; i<= 8 ; i++)
+   { 
+    pinMode(i,OUTPUT);
+   } 
   
 }
 
 void loop()
 {
+ 
  for( int i = 2; i<= 8 ; i++)
-{ 
+  { 
    digitalWrite(i,HIGH);
    delay(analogRead(A0);
    digitalWrite(i,LOW);
- }  
+  }
+  
 }
 
 
